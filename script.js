a30f196d027dab9c272489d571d0977c91c2c310
Ryanzada01 <106113489+Ryanzada01@users.noreply.github.com>
2024-06-11 09:48:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const caixaPrincipal = documnet.querySelector(".caixa-peincipal");
 const caixaPerguntas = documnet.querySelector(".caixa-perguntas");
+const caixaAlternativas = documnet.querySelector(".caixa-alternativas");
+const caixaResultado = documnet.querySelector(".caixa-resultado");
+const textoPrincipal = documnet.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+
+]
