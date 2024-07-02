cb3c3ac0df6d917ce861559d1c64344b2f571f77
Ryanzada01 <106113489+Ryanzada01@users.noreply.github.com>
2024-07-02 09:45:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,6 +18,27 @@ const perguntas = [
             "Alternativa 1",
             "Alternativa 2"
         ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
     }
 
 ]
